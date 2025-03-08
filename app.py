78fa5fc8958dacdf468fcb9dccd6170ab25474fb
titans04 <bandilecele18@gmail.com>
2025-03-08 19:27:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import *
+from flask import *            #imported all necessary libraries to use
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')                  #function for the landing page
 def home():
     return render_template('home.html')
-
-@app.route('/student_dashboard')
+ 
+@app.route('/student_dashboard')     #function for what the logged in student 
 def stud_dashboard():
     return "<h1> this will be a student dashboard </h1>"
 
-@app.route('/admin_dashboard')
+@app.route('/admin_dashboard')        #function for the logged in admin
 def admin_dashboard():
     return "<h1> this will an admin dashbaord </h1>"
     
-@app.route('/login/')
+@app.route('/login/')         
 def login():
     return render_template('login.html')
 
-@app.route('/about')
+@app.route('/about')                    #function for the about page
 def about():
     return "<h1>about page</h1>"
 
-@app.route('/contact')
+@app.route('/contact')                   #function for the contact page
 def contact "<h1> this is for the contact page</h1>"
 
 
