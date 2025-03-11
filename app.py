0a60e17650a68caa3cd754bc5c72f7c641b2bd5c
Mogale Tshehla <102671206+Passsione@users.noreply.github.com>
2025-03-11 11:03:21 +0200

    update

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import *            #imported all necessary libraries to use
+from flask import *
+from pymongo import *
 app = Flask(__name__)
+
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+
+# Database
+# client = pymongo.MongoClient('localhost', 27017)
+# db = client.user_login_system
+
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
+
+# Routes
 
 @app.route('/')                  #function for the landing page
 def home():
     return render_template('home.html')
- 
-@app.route('/student_dashboard')     #function for what the logged in student 
-def stud_dashboard():
-    return "<h1> this will be a student dashboard </h1>"
 
-@app.route('/admin_dashboard')        #function for the logged in admin
-def admin_dashboard():
-    return "<h1> this will an admin dashbaord </h1>"
-    
-@app.route('/login/')         
+@app.route('/login/')
 def login():
     return render_template('login.html')
 
-@app.route('/about')                    #function for the about page
+@app.route('/about/')                    #function for the about page
 def about():
-    return "<h1>about page</h1>"
+    return render_template('about.html')
 
-@app.route('/contact')                   #function for the contact page
-def contact "<h1> this is for the contact page</h1>"
+@app.route('/contact/')                   #function for the contact page
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
