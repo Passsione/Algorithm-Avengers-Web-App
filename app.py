2baa6a9c12e0ec572d0906b4abfbfa7e231a97e9
Mogale Tshehla <102671206+Passsione@users.noreply.github.com>
2025-03-12 20:04:54 +0200

    non-decore

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ from flask import *
 app = Flask(__name__)
 
 
-# Decorators
-def login_required(f):
-  @wraps(f)
-  def wrap(*args, **kwargs):
-    if 'logged_in' in session:
-      return f(*args, **kwargs)
-    else:
-      return redirect('/')
-  
-  return wrap
 
 # Routes
 
