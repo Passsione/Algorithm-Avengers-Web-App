031cb55bc9b22d139a63c389ecea3ce537e34ed5
Mogale Tshehla <102671206+Passsione@users.noreply.github.com>
2025-03-13 01:28:00 +0200

    update3

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import *
-from models import *
-# from functools import wraps
+# from models import *
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///laf_db.db'
 
+# db.init_app(app)
 
 
 # Routes
@@ -28,22 +28,29 @@ def signup_view():
 def student_card():
     return render_template('student_card.html')
 
+@app.route('/Claim/')                   #function for claiming an item page
+def claim():
+    return render_template('item.html')
     
-@app.route('/Searching/')                   #function for searching for a lost item page
+@app.route('/Searching/', methods=['GET', 'POST'])                   #function for searching for a lost item page
 def searching():
+#    if request.method == 'POST':
+#         # sname=request.form['searching'] #retrieves data from form
+#         # new_search = Item(item_name=sname) #creates a new table entry
+#         # db.session.add(new_search)  
+#         # db.session.commit()     #commits to database
+#         return redirect(url_for('claim')) #go to item 
     return render_template('searching.html')
 
 @app.route('/Browse/')                   #function for browsing items page
 def browse():
+    
     return render_template('browse.html')
 
 @app.route('/Report/')                   #function for making a report page
 def report():
     return render_template('report.html')
 
-@app.route('/Claim/')                   #function for claiming an item page
-def claim():
-    return render_template('item.html')
 
 @app.route('/LogIn/')                   #function for the Login page
 def login_view():
