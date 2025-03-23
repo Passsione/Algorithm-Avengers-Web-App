405492567825e7db379fcb5368beca85bda7d776
Mogale Tshehla <102671206+Passsione@users.noreply.github.com>
2025-03-24 00:56:33 +0200

    screener

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,13 +1,28 @@
 
 var body = document.querySelector("body")
-var profile = document.querySelector(".navbar .profile")
+
+var profile =  document.querySelector(".navbar .profile")
 var leaf = profile.querySelector('.flex-col')
 
 
 profile.addEventListener("click", ()=> {        // Open and closes the profile leaf
     leaf.style.display == 'none' ? leaf.style.display ='flex' : leaf.style.display = 'none'
 })
-body.addEventListener("click", ()=>{
-    if(leaf.style.display != 'none')leaf.style.display == 'none'
-})
+
+
+// var adminTasks = document.querySelectorAll(".container .admin-dash")
+// var container = adminTasks[0].parentElement
+// var action = document.querySelectorAll(".container .acts")
+
+//     adminTasks.forEach( (at) => {
+//         at.addEventListener("click", () =>{
+//             if(container.dataset.tab != at.id){
+//                 at.style.display = 'none'
+//             }else{at.style.display = 'grid'}
+//         })
+//     });
+
+//     action.addEventListener("click", () =>{
+        
+//     })
 
